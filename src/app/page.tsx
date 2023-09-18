c8ea503519a4f772d494c988e8d4410e139ba219
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-09-18 16:05:51 +0300

    feat: set page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col">
       <HeroSection />
-      <div>this is the main page</div>
+      <div
+        className="flex flex-col  bg-red-300
+      xl:w-[1200px] lg:w-[1000px] lg:mx-auto w-full px-4
+      "
+      >
+        <div>this is experience section</div>
+      </div>
     </main>
   );
 }
