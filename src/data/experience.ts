299b9cfca5e9b2d61c8dc6bf69742c2ff30ce01b
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-09-19 14:43:15 +0300

    feat: add experience data

---
+++ src/data/experience.ts
@@ -0,0 +1,77 @@
+export const experience = [
+  {
+    companyName: "Windranger Labs",
+    startDate: "February 01, 2022 00:00:00",
+    endDate: "August 01, 2023 00:00:00",
+    role: "Front-End Developer",
+    jobType: "Remote",
+    description:
+      "Prticipated in building Mantle token (former BitDAO). Developed new features for their websites and optimize code. Helped to manage directus CMS.",
+    skills: [
+      "Next js",
+      "React js",
+      "Typescript",
+      "REST APIs",
+      "Graphql",
+      "web3",
+      "CMS",
+      "Tailwind",
+    ],
+  },
+  {
+    companyName: "Informatics Services Corporation",
+    startDate: "July 01, 2021 00:00:00",
+    endDate: "December 01, 2022 00:00:00",
+    role: "Front-End Developer",
+    jobType: "Hybrid",
+    description:
+      "Participate in building first cryptocurrency in the country for the Central Bank of Iran",
+    skills: [
+      "Next js",
+      "React js",
+      "Typescript",
+      "REST APIs",
+      "Webpack",
+      "Redux",
+      "React Native",
+    ],
+  },
+  {
+    companyName: "Stage Try",
+    startDate: "March 01, 2021 00:00:00",
+    endDate: "June 01, 2021 00:00:00",
+    role: "Front-End Developer",
+    jobType: "Remote",
+    description:
+      "A Startup creating a kind of online shop mixing the idea of video making for the products. I helped with front end side of website and mnaging pannel",
+    skills: [
+      "Next js",
+      "React js",
+      "Typescript",
+      "REST APIs",
+      "Webpack",
+      "Redux",
+      "Javascript",
+    ],
+  },
+  {
+    companyName: "SRICO (Soroosh Rayaneh Iranian)",
+    startDate: "January 01, 2020 00:00:00",
+    endDate: "March 01, 2021 00:00:00",
+    role: "Front-End Developer",
+    jobType: "Hybrid",
+    description:
+      "Creating websites for scientific purposes. I developed various charts and maps and developed pannels for scientists",
+    skills: ["React js", "Javascript", "REST APIs", "Webpack", "Redux"],
+  },
+  {
+    companyName: "Rayan Gostar Novin Pardazan Bakhtar",
+    startDate: "September 01, 2015 00:00:00",
+    endDate: "December 01, 2019 00:00:00",
+    role: "Front-End Developer",
+    jobType: "On-Site",
+    description:
+      "worked closely with business leaders and monitored website performance and rectifying front-end issues.Turned design pages and specific components into HTML, CSS and Javascript.",
+    skills: ["HTML", "Javascript", "CSS", "SASS"],
+  },
+];
