c259261dd4e78c5779a75617e34600e0f634b3de
Tannaz Shirzadi <tia.shr97@gmail.com>
2023-10-17 17:03:52 +0330

    feat: display real skills in chart

--- src/components/SkillsSection/BarChart.tsx
+++ src/components/SkillsSection/BarChart.tsx
@@ -1,43 +1,47 @@
 "use client";
+import { skills } from "@/data/skills";
+import { ISkill } from "@/models/general";
 import { useEffect } from "react";
 
 // import "tw-elements/dist/css/tw-elements.min.css";
 
 function BarChart() {
+  const extractChartLabels = () => {
+    const names = skills.flatMap((skill: ISkill) => skill.name);
+    return names;
+  };
+  const extractChartdata = () => {
+    const years = skills.flatMap((skill: ISkill) => skill.years);
+    return years;
+  };
+  const dataBar = {
+    type: "bar",
+    data: {
+      labels: [...extractChartLabels()],
+      datasets: [
+        {
+          label: "Skill (years)",
+          data: [...extractChartdata()],
+        },
+      ],
+    },
+  };
   useEffect(() => {
+    const years = extractChartdata();
     const init = async () => {
       const { Chart } = await import("tw-elements");
-      const dataBar = {
-        type: "bar",
-        data: {
-          labels: [
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-            "Sunday ",
-          ],
-          datasets: [
-            {
-              label: "Traffic",
-              data: [2112, 2343, 2545, 3423, 2365, 1985, 987],
-            },
-          ],
-        },
-      };
       const myBarChart = new Chart(
         document.getElementById("myBarChart"),
         dataBar
       );
     };
     init();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
     <>
-      <div className="mx-auto w-3/5 overflow-hidden">
+      <div className="mx-auto lg:w-2/3 w-full overflow-hidden h-[25rem] bg-primary-teal p-2 rounded-md border border-primary-navy">
         <canvas id="myBarChart"></canvas>
       </div>
     </>
